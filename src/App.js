2da79306661fd27d1ad4b0db1ae8fcbeebdf2507
vndrewhk <drewhkm@gmail.com>
2022-01-27 23:30:11 -0500

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Button from "@mui/material/Button";
 import Header from "./components/Header/Header";
 import Main from "./components/Main-Page/main";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
       {/* <Button variant="contained">Hello World</Button> */}
     </>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+
+import { Typography } from "@mui/material";
+// import styles from "./header.module.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const Footer = () => {
+  return (
+    <div >
+      {/*  <div className = {styles['header-box']}> */}
+      <Typography variant="p1" component="h2">
+        Built using React.js. 
+         
+      </Typography>
+
+    </div>
+  );
+};
+
+export default Footer;
